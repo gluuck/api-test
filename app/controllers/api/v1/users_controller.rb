d3cf8d3effd28b612eec941d51b54d59d8af7c30
gluuck <ostapenkov74@gmail.com>
2021-05-03 11:23:36 +0500

    add methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,21 +16,38 @@ module Api
       end
 
       def edit
-        
+        render json: @user
       end
 
       def create
+        @user = User.new(user_params)
+        if @user.save
+          render json: @user
+        else
+          render json: @user.errors, status: :unprocessable_entity 
+        end
       end
 
       def update
+        if @user.update(user_params)
+          render json: @user
+        else
+          render json: @user.errors, status: :unprocessable_entity 
+        end
       end
 
       def destroy
+        @user.destroy
+        render json[:notice]='User was destroyed' 
       end
 
       private
       def set_user
         @user = User.find(params[:id])
+      end
+
+      def user_params
+        params.fetch(:user,{}).permit(:name, :email,:password)
       end
     end
   end  
