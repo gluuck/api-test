de9058149beb4981990f10222d3b850a9d904ae5
gluuck <ostapenkov74@gmail.com>
2021-05-03 09:45:25 +0500

    validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  #get 'users/index'
-  #get 'users/show'
-  #get 'users/new'
-  #get 'users/edit'
-  #get 'users/create'
-  #get 'users/update'
-  #get 'users/destroy'
+  
   namespace  :api do
     namespace  :v1 do
       resources  :users
