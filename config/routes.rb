b8eb0b8729ac4d7434d4092f0d665f11824c7c94
gluuck <ostapenkov74@gmail.com>
2021-05-03 09:27:17 +0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/create'
-  get 'users/update'
-  get 'users/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get 'users/index'
+  #get 'users/show'
+  #get 'users/new'
+  #get 'users/edit'
+  #get 'users/create'
+  #get 'users/update'
+  #get 'users/destroy'
+  namespace  :api do
+    namespace  :v1 do
+      resources  :users
+    end
+  end
+  
 end
