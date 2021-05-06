15c16b63588aef3d2ee9c0d790109e0f0ad02981
gluuck <ostapenkov74@gmail.com>
2021-05-06 09:35:04 +0500

    first test & controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class UsersController < ApplicationController
+
       before_action :set_user, only: %i[show edit update destroy]
 
       def index
-        @users = User.all
+        @users = User.limit(10)
         render json: @users
       end
 
@@ -17,15 +20,14 @@ module Api
         render json: @user
       end
 
-      def edit        
-      end
+      def edit; end
 
       def create
         @user = User.new(user_params)
         if @user.save
           render json: @user
         else
-          render json:{error: 'User wasn\'t create'} 
+          render json: {status: "error", message: 'User wasn\'t create' }
         end
       end
 
@@ -33,27 +35,32 @@ module Api
         if @user.update(user_params)
           render json: @user
         else
-          render json:{error: 'User wasn\'t update'} 
+          render json: {status: "error", message: 'User wasn\'t update' }
         end
       end
 
       def destroy
-        @user.destroy
-        render json: {notice: 'User was destroyed'} 
+        if @user.destroy
+          render json: { notice: 'User was destroyed' }
+        else
+          render json: {status: "error", message: 'The user wasn\'t deleted' }
+        end
       end
 
       def login
         @user = User.find_by(:email)
         render json: @user if @user.value?(@user.password)
       end
+
       private
+
       def set_user
         @user = User.find(params[:id])
       end
+
       def user_params
-        params.fetch(:user,{}).permit(:name, :email,:password)
+        params.require(:user).permit(:name, :email, :password, :password_digest)
       end
     end
-  end  
+  end
 end
-
--- spec/models/user_model_spec.rb
+++ spec/models/user_model_spec.rb
@@ -1,8 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe User , type: :model do
-  it 'should not empty' do
-  user=User.new(name: 'Vasya',email:'vasya@gmail.com',password:'123123')  
-  expect(user).to be_valid  
-  end  
+RSpec.describe User, type: :model do  
+  subject {FactoryBot.build(:user)}    
+  it { expect(subject).to be_valid }   
 end
